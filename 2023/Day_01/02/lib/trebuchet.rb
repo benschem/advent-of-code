02f2c078b2ba102e2106bde6a2a68ebae6caac3b
Ben Schembri <benschembri@gmail.com>
2023-12-04 16:32:55 +1100

    solved day1 puzzle2

--- 2023/Day_01/02/lib/trebuchet.rb
+++ 2023/Day_01/02/lib/trebuchet.rb
@@ -1,28 +1,37 @@
-# Each line originally contained a specific calibration value.
-# On each line, the calibration value can be found by combining the first digit
-# and the last digit (in that order) to form a single two-digit number.
+NUMBERS_AS_WORDS = %w[one two three four five six seven eight nine]
+NUMBERS_AS_STRINGS = %w[1 2 3 4 5 6 7 8 9]
 
-# For example:
+def convert_words_to_numbers(digits)
+  NUMBERS_AS_WORDS.each_with_index do |word, index|
+    digits.each do |digit_hash|
+      digit_hash[:number].gsub!(word, (index + 1).to_s)
+    end
+  end
 
-# 1abc2
-# pqr3stu8vwx
-# a1b2c3d4e5f
-# treb7uchet
-# In this example, the calibration values of these four lines are 12, 38, 15,
-# and 77. Adding these together produces 142.
+  digits
+end
 
-# What is the sum of all of the calibration values?
-
-# For each line
-# Find the first digit
-# Find the second digit
-# Combine them to form one number
-# Add number to the total
-# end
+def check_for_numbers(string, numbers)
+  order_of_numbers = []
+  numbers.each do |number|
+    string.scan(/#{number}/) do |match|
+      order_of_numbers << { position: Regexp.last_match.begin(0), number: match }
+    end
+  end
+  order_of_numbers
+end
 
 def get_first_and_last_digits(string)
-  string.delete!('^0-9')
-  (string[0] + string[-1]).to_i
+  order_of_numbers = []
+  order_of_numbers << check_for_numbers(string, NUMBERS_AS_WORDS)
+  order_of_numbers << check_for_numbers(string, NUMBERS_AS_STRINGS)
+  order_of_numbers.flatten!
+
+  digits = order_of_numbers.sort_by { |hash| hash[:position] }
+
+  convert_words_to_numbers(digits)
+
+  (digits.first[:number] + digits.last[:number]).to_i
 end
 
 def trebuchet(data)
--- 2023/Day_01/02/spec/trebuchet_spec.rb
+++ 2023/Day_01/02/spec/trebuchet_spec.rb
@@ -1,31 +1,39 @@
 require 'trebuchet'
 
-test_data = %w[1abc2 pqr3stu8vwx a1b2c3d4e5f treb7uchet]
+test_data = %w[two1nine eightwothree abcone2threexyz xtwone3four 4nineeightseven2 zoneight234 7pqrstsixteen]
 
 describe '#get_first_and_last_digits' do
-  it 'should return the first and last digits' do
-    expect(get_first_and_last_digits(test_data[0])).to eq 12
+  it 'should return the first and last digits even if they are words' do
+    expect(get_first_and_last_digits(test_data[0])).to eq 29
   end
 
-  it 'should return an integer' do
-    expect(get_first_and_last_digits(test_data[0])).to be_a Integer
+  it 'should still return an integer even when given no digits' do
+    expect(get_first_and_last_digits(test_data[1])).to be_a Integer
   end
 
-  it 'should return the first and last digits even with letters in front of them' do
-    expect(get_first_and_last_digits(test_data[1])).to eq 38
+  it 'should return the first and last digits even with extra letters' do
+    expect(get_first_and_last_digits(test_data[2])).to eq 13
   end
 
-  it 'should return the first and last digits even with multiple digits' do
-    expect(get_first_and_last_digits(test_data[2])).to eq 15
+  it 'should return the correct first and last digits even with multiple words' do
+    expect(get_first_and_last_digits(test_data[3])).to eq 24
   end
 
-  it 'should return the first digit twice if there is only one digit' do
-    expect(get_first_and_last_digits(test_data[3])).to eq 77
+  it 'should return the first and last digits despite having words between digits' do
+    expect(get_first_and_last_digits(test_data[4])).to eq 42
+  end
+
+  it 'should return the correct first and last digits' do
+    expect(get_first_and_last_digits(test_data[5])).to eq 14
+  end
+
+  it 'should return the correct first and last digits' do
+    expect(get_first_and_last_digits(test_data[6])).to eq 76
   end
 end
 
 describe '#trebuchet' do
   it 'should return the total value of the digits on each line' do
-    expect(trebuchet(test_data)).to eq(142)
+    expect(trebuchet(test_data)).to eq(281)
   end
 end
